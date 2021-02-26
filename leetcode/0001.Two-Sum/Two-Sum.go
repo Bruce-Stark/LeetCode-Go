092c1163734b0c8664f3af0f89d7397db712f9da
Bruce-Stark <1052882558@qq.com>
2021-02-26 09:38:16 +0800

    2.26

--- leetcode/0001.Two-Sum/Two-Sum.go
+++ leetcode/0001.Two-Sum/Two-Sum.go
@@ -1,1 +1,23 @@
 package leetcode
+
+func cusTwoSum(nums []int, target int) []int {
+	m := make(map[int]int)
+	for k, v := range nums {
+		if idx, ok := m[target-v]; ok {
+			return []int{idx, k}
+		}
+		m[v] = k
+	}
+	return nil
+}
+
+func cus1(nums []int, target int) []int {
+	m := make(map[int]int)
+	for k, v := range nums {
+		if idx, ok := m[target-v]; ok {
+			return []int{idx, k}
+		}
+		m[v] = k
+	}
+	return nil
+}
